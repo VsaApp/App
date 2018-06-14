8d98c61a687adc801f86bc0b02c513d77f4a6907
jld3103 <jld3103yt@gmail.com>
2018-06-14 20:46:38 +0100

    Changed day start time

--- VsaApp/app/src/main/java/de/lohl1kohl/vsaapp/LessonUtils.java
+++ VsaApp/app/src/main/java/de/lohl1kohl/vsaapp/LessonUtils.java
@@ -10,18 +10,18 @@ public class LessonUtils {
     private static int[] endTimes = new int[]{60, 130, 210, 280, 360, 420, 480, 545};
     private static List<String> weekdays;
 
-    public static boolean isDayInFuture(String day){
+    public static boolean isDayInFuture(String day) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(new Date());
         int weekday = calendar.get(Calendar.DAY_OF_WEEK) - 2;
         return weekdays.indexOf(day) <= weekday;
     }
 
-    public static boolean isLessonPassed(int unit){
+    public static boolean isLessonPassed(int unit) {
         return getEndTime(unit) < getTimePassed();
     }
 
-    public static void setWeekdays(List<String> days){
+    public static void setWeekdays(List<String> days) {
         weekdays = days;
     }
 
@@ -35,7 +35,7 @@ public class LessonUtils {
 
     private static long getTimePassed() {
         Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.HOUR_OF_DAY, 9);
+        cal.set(Calendar.HOUR_OF_DAY, 8);
         Date startDate = cal.getTime();
         long diff = new Date().getTime() - startDate.getTime();
         return diff / 1000 / 60;
