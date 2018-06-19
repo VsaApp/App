a1b08f0a5f0fbd35464dd51fefbea3eec3ba2ff5
jld3103 <jld3103yt@gmail.com>
2018-06-19 18:14:17 +0200

    Added message filtering for notifications

--- VsaApp/app/src/main/java/de/lohl1kohl/vsaapp/FirebaseMessagingService.java
+++ VsaApp/app/src/main/java/de/lohl1kohl/vsaapp/FirebaseMessagingService.java
@@ -4,6 +4,8 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -13,53 +15,83 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Random;
 
 public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
 
-    private int channel = 0;
+    private Map<String, String> subjectsSymbols = new HashMap<>();
+
+    @Override
+    public void onCreate() {
+        String[] subjects = getResources().getStringArray(R.array.nameOfSubjects);
+        for (String subject : subjects) {
+            String[] pair = subject.split(":");
+
+            subjectsSymbols.put(pair[0], pair[1]);
+        }
+    }
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
         try {
-            String title = "";
-            String text = "";
+            String title;
+            StringBuilder text = new StringBuilder();
             JSONObject jsonObject = new JSONObject(remoteMessage.getData().get("data"));
             Log.i("JSON", jsonObject.toString());
             String weekday = jsonObject.getString("weekday");
             JSONArray changes = jsonObject.getJSONArray("changes");
-            if (changes.length() > 1) {
-                title = weekday;
-                for (int i = 0; i < changes.length(); i++) {
-                    JSONObject change = changes.getJSONObject(i);
-                    Log.i("Change", change.toString());
-                    text += change.getString("unit") + ". Stunde " + change.getJSONObject("changed").getString("tutor") + " " + change.getJSONObject("changed").getString("info") + " " + change.getJSONObject("changed").getString("room") + "\n";
+            SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+            for (int i = 0; i < changes.length(); i++) {
+                JSONObject change = changes.getJSONObject(i);
+                String prefName = String.format("pref_selectedSubject%s:%s:%s", settings.getString("pref_grade", "-1"), weekday, Integer.parseInt(change.getString("unit")) - 1);
+                String prefValue = settings.getString(prefName, "-1");
+                boolean display = false;
+                if (!prefValue.equals("-1")) {
+                    Log.i(prefName, prefValue);
+                    String subject = prefValue.split(":")[0];
+                    String tutor = prefValue.split(":")[1];
+                    String changedTutor = change.getJSONObject("changed").getString("tutor");
+                    if (changedTutor.equals("")) {
+                        changedTutor = tutor;
+                    }
+                    Log.i(tutor, changedTutor);
+                    Log.i(change.getString("lesson"), subject);
+                    if (changedTutor.equals(tutor) && change.getString("lesson").equals(subject)) {
+                        display = true;
+                    }
                 }
-                text = text.substring(0, text.length() - 1);
-            } else {
-                title = weekday + " " + changes.getJSONObject(0).getString("unit") + ". Stunde";
-                text = changes.getJSONObject(0).getJSONObject("changed").getString("tutor") + " " + changes.getJSONObject(0).getJSONObject("changed").getString("info") + " " + changes.getJSONObject(0).getJSONObject("changed").getString("room");
+                if (display) {
+                    text.append(change.getString("unit")).append(". Stunde ").append(change.getJSONObject("changed").getString("tutor")).append(" ").append(change.getJSONObject("changed").getString("info")).append(" ").append(change.getJSONObject("changed").getString("room")).append("\n");
+                }
             }
-            Intent intent = new Intent(this, MainActivity.class);
-            intent.putExtra("page", "vp");
-            intent.putExtra("day", weekday);
-            Random generator = new Random();
 
-            PendingIntent i = PendingIntent.getActivity(getApplicationContext(), generator.nextInt(), intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            title = weekday;
+            if (text.length() != 0) {
+                text = new StringBuilder(text.substring(0, text.length() - 1));
 
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), String.valueOf(channel))
-                    .setSmallIcon(R.mipmap.logo_white)
-                    .setContentTitle(title)
-                    .setContentText(text.split("\n")[0])
-                    .setColor(getResources().getColor(R.color.colorPrimary))
-                    .setStyle(new NotificationCompat.BigTextStyle().bigText(text))
-                    .setVibrate(new long[]{500, 500})
-                    .setContentIntent(i);
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.putExtra("page", "vp");
+                intent.putExtra("day", weekday);
+                Random generator = new Random();
 
-            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            Objects.requireNonNull(notificationManager).notify(channel++, builder.build());
+                PendingIntent i = PendingIntent.getActivity(getApplicationContext(), generator.nextInt(), intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), String.valueOf(generator.nextInt()))
+                        .setSmallIcon(R.mipmap.logo_white)
+                        .setContentTitle(title)
+                        .setContentText(text.toString().split("\n")[0])
+                        .setColor(getResources().getColor(R.color.colorPrimary))
+                        .setStyle(new NotificationCompat.BigTextStyle().bigText(text.toString()))
+                        .setVibrate(new long[]{500, 500})
+                        .setContentIntent(i);
+
+                NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                Objects.requireNonNull(notificationManager).notify(generator.nextInt(), builder.build());
+            }
         } catch (JSONException e) {
             e.printStackTrace();
         }
