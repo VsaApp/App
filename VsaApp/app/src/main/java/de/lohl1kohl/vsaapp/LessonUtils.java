449f01a8c1a194cdaede067ebf5542778277c91c
Unknown <tupfi2@gmx.de>
2018-08-31 22:25:33 +0200

    When it's friday evening is nothing gray

--- VsaApp/app/src/main/java/de/lohl1kohl/vsaapp/LessonUtils.java
+++ VsaApp/app/src/main/java/de/lohl1kohl/vsaapp/LessonUtils.java
@@ -4,6 +4,8 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
+import de.lohl1kohl.vsaapp.holder.SpHolder;
+
 public class LessonUtils {
 
     public static int[] endTimes = new int[]{60, 130, 210, 280, 360, 420, 480, 545};
@@ -14,7 +16,7 @@ public class LessonUtils {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(new Date());
         int weekday = calendar.get(Calendar.DAY_OF_WEEK) - 2;
-        if (weekday == -1 | weekday == 5) weekday = 0;
+        if (weekday == -1 | weekday == 5 | isFridayEvening()) weekday = 0;
         return weekdays.indexOf(day) < weekday;
     }
 
@@ -22,8 +24,23 @@ public class LessonUtils {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(new Date());
         int weekday = calendar.get(Calendar.DAY_OF_WEEK) - 2;
-        if (weekday == 5) weekday = -1;
+        if (weekday == 5 | isFridayEvening()) weekday = -1;
         return weekdays.indexOf(day) > weekday;
+    }
+
+    private static boolean isFridayEvening(){
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        int weekday = calendar.get(Calendar.DAY_OF_WEEK);
+        if (weekday == 6) {
+            return isSchoolAtDayPassed(4);
+        }
+        return false;
+    }
+
+    private static boolean isSchoolAtDayPassed(int day){
+        int lastLesson = SpHolder.getDay(day).size() - 1;
+        return isLessonPassed(lastLesson);
     }
 
     public static boolean isLessonPassed(int unit) {
